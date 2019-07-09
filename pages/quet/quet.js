0e8cdf92911fb6bb7ad33344ba6b350fb0342c65
liuhaow <1258596443@qq.com>
2019-07-09 13:44:01 +0800

    最新版

--- pages/quet/quet.js
+++ pages/quet/quet.js
@@ -5,67 +5,95 @@ Page({
   data: {
     animation: '',
     courinde:'4',
+    index:0,
+    flag:3,
+    sure:false,
+    anser:'',
     list: [
       {
-        name: 'A.湿垃圾'
+        question: "彩虹六号：围攻总共出了",
+        answers: ['A', 'B', 'C', 'D'],
+        right_answer: "C"
       },
       {
-        name: 'B.干垃圾'
+        question: "截至风城行动，彩虹六号：",
+        answers: ['A', 'B', 'C', 'D'],
+        right_answer: "D"
       },
       {
-        name: 'C.可回收垃圾'
+        question: "截围攻总共出了多少个特勤干员？",
+        answers: ['A', 'B', 'C', 'D'],
+
+        right_answer: "A"
       },
       {
-        name: 'D.其他垃圾'
+        question: "截？",
+        answers: ['A', 'B', 'C', 'D'],
+        right_answer: "C"
+      },
+      {
+        question: "截至员？",
+        answers: ['A', 'B', 'C', 'D'],
+        right_answer: "C"
       }
+
     ]
   },
-  onTabsItem(e){
-console.log(e)
-var that =this
-    var index = e.currentTarget.dataset.index;
-    that.setData({
-      courinde :index
-    })
 
-  },
   onShow: function() {
-    _animation = wx.createAnimation({
-      duration: _ANIMATION_TIME,
-      timingFunction: 'linear', // "linear","ease","ease-in","ease-in-out","ease-out","step-start","step-end"
-      delay: 0,
-      transformOrigin: '50% 50% 0'
-    })
-  },
 
-  /**
-   * 实现image旋转动画，每次旋转 120*n度
-   */
-  rotateAni: function(n) {
-    _animation.rotate(120 * (n)).step()
-    this.setData({
-      animation: _animation.export()
-    })
   },
-
-  /**
-   * 开始旋转
-   */
-  startAnimationInterval: function() {
+  choseInfo(e){
     var that = this;
-    that.rotateAni(++_loadImagePathIndex); // 进行一次旋转
-    _animationIntervalId = setInterval(function() {
-      that.rotateAni(++_loadImagePathIndex);
-    }, _ANIMATION_TIME); // 没间隔_ANIMATION_TIME进行一次旋转
+    var anst= e.currentTarget.dataset.anser;
+    that.setData({
+      anser: anst
+    })
   },
+  btnque(){
+    console.log(1215)
+    this.setData({
+      sure:true
+    })
+    var right_answer = this.data.list[this.data.index].right_answer;
+    console.log(right_answer)
+    console.log(this.data.anser)
+    if (this.data.anser == right_answer ){
+      console.log(this.data.list.length)
+      
 
-  /**
-   * 停止旋转
-   */
-  stopAnimationInterval: function() {
-    if (_animationIntervalId > 0) {
-      clearInterval(_animationIntervalId);
-      _animationIntervalId = 0;
+    }else{
+      console.log(2)
+      return
+
+    }
+    if (this.data.index < this.data.list.length - 1) {
+      this.NextQuestion();
+    }
+    //最后一道题
+    else if (this.data.index == this.data.questionnum - 1) {
+      this.setData({
+        canAnswer: 'true'//禁止答题
+      })
     }
   },
+  NextQuestion: function () {
+    //不是最后一道题
+    if (this.data.index < this.data.list.length - 1) {
+      this.setData({
+        index: this.data.index + 1,
+      })
+    }
+    //最后一道题
+    if (this.data.index == this.data.list.length - 1) {
+      this.setData({
+        index: this.data.index,
+      })
+    }
+  },
+
+  
+
+
+
 })
